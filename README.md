46593b3f8cc100a846da128873bc7fe51a41d7e3
kushalv238 <kushalv238@gmail.com>
2024-05-05 21:21:33 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Steps to visulize the multimodel file
+
+1. Creating a virtual environment
+```bash
+python -m venv venv
+```
+
+2. Activate the virtual environment
+```bash
+.\venv\Scripts\activate
+```
+
+2. Downloading the requirments
+```bash
+pip install -r requiremnts.txt
+```
+
+3. Run the [`MentalHealth_Prediction.ipynb`](https://github.com/kushalv238/MentalHealth_research/tree/main/code/multimodel_prediction/MentalHealth_Prediction.ipynb) file
+
+4. Run the flask app ie [`app.py`](https://github.com/kushalv238/MentalHealth_research/tree/main/code/multimodel_prediction/Web_page/app.py) file
+
+```bash
+python app.py
+```
+
+5. View the webpage on port
+```
+http://127.0.0.1:5000
+```
---
+++ Web_page/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__, template_folder="template")
+# import warnings
+# from sklearn.exceptions import InconsistentVersionWarning
+
+# # Suppress InconsistentVersionWarning
+# warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
+
+
+model = joblib.load(
+    "adaboost_model.pkl"
+)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST", "GET"])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction = model.predict_proba(final)
+    output = "{0:.{1}f}".format(prediction[0][1], 2)
+
+    if output > str(0.5):
+        return render_template(
+            "index.html",
+            pred="You need a treatment.\nProbability of mental illness is {}".format(
+                output
+            ),
+        )
+    else:
+        return render_template(
+            "index.html",
+            pred="You do not need treatment.\n Probability of mental illness is {}".format(
+                output
+            ),
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
